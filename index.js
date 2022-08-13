cf54bb0b72e74ce154725975950fa7f4f59564c1
RimmaBuzuluk <rimmysya2000@gmail.com>
2022-08-13 16:00:23 +0300

    2 commit

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 class Worker {
-  name;
-  surname;
-  rate;
-  days;
+  _name;
+  _surname;
+  _rate;
+  _days;
 
-  constructor(n, s, r, d) {
-    this.name = n;
-    this.surname = s;
-    this.rate = r;
-    this.days = d;
+  constructor(name, surname, rate, days) {
+    this._name = name;
+    this._surname = surname;
+    this._rate = rate;
+    this._days = days;
   }
   getSalary() {
-    return this.rate * this.days;
+    return this._rate * this._days;
   }
 }
 
 var worker = new Worker("Иван", "Иванов", 10, 31);
 var worker2 = new Worker("Дмитро", "Петров", 12, 25);
 
-console.log(worker.name); //выведет 'Иван'
-console.log(worker.surname); //выведет 'Иванов'
-console.log(worker.rate); //выведет 10
-console.log(worker.days); //выведет 31
+console.log(worker._name); //выведет 'Иван'
+console.log(worker._surname); //выведет 'Иванов'
+console.log(worker._rate); //выведет 10
+console.log(worker._days); //выведет 31
 console.log(worker.getSalary());
 console.log(worker.getSalary() + worker2.getSalary());
